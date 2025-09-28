0f5e8bd324698f4f3ab5a8972c58880d872a7477
amaancoderx <amaan.coderx@gmail.com>
2025-09-28 13:38:40 +0530

    Fix Prisma generate for Vercel

---
+++ goat-notes-main/src/app/page.tsx
@@ -0,0 +1,45 @@
+import { getUser } from "@/auth/server";
+import AskAIButton from "@/components/AskAIButton";
+import NewNoteButton from "@/components/NewNoteButton";
+import NoteTextInput from "@/components/NoteTextInput";
+import HomeToast from "@/components/HomeToast";
+import { prisma } from "@/db/prisma";
+
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+async function HomePage({ searchParams }: Props) {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
+  const noteId = Array.isArray(noteIdParam)
+    ? noteIdParam![0]
+    : noteIdParam || "";
+
+  let note = null;
+  if (user && noteId) {
+    try {
+      note = await prisma.note.findUnique({
+        where: { id: noteId, authorId: user.id },
+      });
+    } catch (error) {
+      console.error("Database connection failed:", error);
+    }
+  }
+
+  return (
+    <div className="flex h-full flex-col items-center gap-4">
+      <div className="flex w-full max-w-4xl justify-end gap-2">
+        <AskAIButton user={user} />
+        <NewNoteButton user={user} />
+      </div>
+
+      <NoteTextInput noteId={noteId} startingNoteText={note?.text || ""} />
+
+      <HomeToast />
+    </div>
+  );
+}
+
+export default HomePage;
